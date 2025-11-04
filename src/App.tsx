c3ad21dfa4a0d7e63e567c273e9f6fd4c816250e
devxyn <johnlawrencehermo@gmail.com>
2025-11-05 01:15:19 +0800

    feat: integrate Details, Map, and Navbar components within MapProvider in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import Details from "./components/Details";
+import Map from "./components/Map";
+import Navbar from "./components/Navbar";
+import { MapProvider } from "./context/MapContext";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <MapProvider>
+      <main>
+        <Navbar />
+        <section className='flex flex-col md:flex-row w-full h-dvh pt-[80px]'>
+          <Details />
+          <Map />
+        </section>
+      </main>
+    </MapProvider>
+  );
 };
 
 export default App;
