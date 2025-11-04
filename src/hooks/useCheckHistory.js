4b468c77af1225ccca10ec04de66bdef86fa72cc
devxyn <johnlawrencehermo@gmail.com>
2025-11-04 17:39:47 +0800

    chore: remove all project files and configurations

--- src/hooks/useCheckHistory.js
+++
@@ -1,7 +0,0 @@
-const useCheckHistory = () => {
-  const savedItem = localStorage.getItem('history');
-  const savedHistory = JSON.parse(savedItem);
-  return savedHistory || []; // Initialize with saved data or an empty array
-};
-
-export default useCheckHistory;
