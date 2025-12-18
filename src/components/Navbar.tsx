ba8b713247ec4f8108a8e000716274129a8472b1
devxyn <johnlawrencehermo@gmail.com>
2025-12-19 01:02:23 +0800

    feat: updated developed by and github url

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,14 @@
 const Navbar = () => {
   return (
-    <header className='h-[80px] w-full px-10 border-b border-light-border lg:fixed'>
+    <header className='h-20 w-full px-10 border-b border-light-border lg:fixed'>
       <nav className='h-full flex flex-col xs:flex-row justify-center xs:justify-between items-center gap-2'>
         <a href='/' className='flex flex-row items-center text-3xl font-bold'>
           <img src='/logo.png' alt='logo' className='h-5 w-5 mr-2' />
           <h1>Find My Ip</h1>
         </a>
 
-        <a className='text-xs' href='http://www.github.com/devxyn' target='_blank' rel='noopener noreferrer'>
-          Developed by: <span className='font-bold'>devxyn</span>
+        <a className='text-xs' href='http://www.github.com/hermooo' target='_blank' rel='noopener noreferrer'>
+          Developed by: <span className='font-bold'>hermooo</span>
         </a>
       </nav>
     </header>
